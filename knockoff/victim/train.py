6df5edf1876bb177f2d5d7df9a4b5509d086127f
Nikhil Chilwant <nikhilbchilwant@gmail.com>
2020-01-12 21:29:35 +0100

    Cleaned code and ran successfully.
    Use /knockoffnets/knockoff/victim/train.py AG_NEWS wordembedding -d -1
    -o models/victim/VIC_DIR -e 10 to run the code.

--- knockoff/victim/train.py
+++ knockoff/victim/train.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
-"""This is a short description.
-Replace this with a more detailed description of what this file contains.
+"""Trains and stores the victim model. We use it later to generate transfer set.
 """
 import argparse
 import json
@@ -64,62 +63,38 @@ def main():
 	params = vars(args)
 
 	# torch.manual_seed(cfg.DEFAULT_SEED)
+	# extract parameter arguments into variables
+	embed_dim = params['embed_dim']
+	dataset_name = params['dataset']
+	datadir = params['datadir']
+	out_path = params['out_path']
+	batch_size = params['batch_size']
+	lr = params['lr']
+	lr_gamma = params['lr_gamma']
+	num_workers = params['num_workers']
+	num_epochs = params['epochs']
+	model_name = params['model_arch']
+	pretrained = params['pretrained']
+
 	if params['device_id'] >= 0:
 		os.environ["CUDA_VISIBLE_DEVICES"] = str(params['device_id'])
 		device = torch.device('cuda')
 	else:
 		device = torch.device('cpu')
 
-	embed_dim = params['embed_dim']
-	# ----------- Set up dataset
-	# Checks validity of provided parameters
-	dataset_name = params['dataset']
-	datadir = params['datadir']
-	out_path = params['out_path']
-	batch_size = params['batch_size']
-	lr = params['lr']
-	# lr_step = params['lr-step']
-	lr_gamma = params['lr_gamma']
-	num_workers = params['num_workers']
-	num_epochs = params['epochs']
-
-	# valid_datasets = datasets.__dict__.keys() #gives: dict_keys(['__file__', 'SVHN', '__spec__', '__cached__', '__path__', 'cifarlike', 'modelfamily_to_transforms', 'diabetic5', 'KMNIST', 'caltech256', 'Caltech256', '__doc__', 'dataset_to_modelfamily', 'Indoor67', 'transforms', '__package__', 'CIFAR10', 'EMNISTLetters', '__name__', 'indoor67', 'tinyimagenet200', 'Diabetic5', 'EMNIST', 'mnistlike', 'MNIST', '__builtins__', 'TinyImageNet200', 'FashionMNIST', 'ImageNet1k', 'imagenet1k', 'CIFAR100', 'cubs200', 'CUBS200', '__loader__'])
+	# Currently supports only the torchtext datasets
 	valid_datasets = list(text_classification.DATASETS.keys())
 	if dataset_name not in valid_datasets:
 		raise ValueError('Dataset not found. Valid arguments = {}'.format(valid_datasets))
-	# dataset = datasets.__dict__[dataset_name]
 
+	# Currently, we will support classification tasks only. Sentiment task is the next one.
+	# See /knockoff/datasets/__init__.py for mapping
+	modelfamily = datasets.dataset_to_modelfamily[dataset_name]  # e.g. 'classification'
+	metadata = datasets.dataset_metadata[dataset_name]  # Relevant parameters for the task. e.g. 'ngram'
 
-	# TEXT = data.Field(lower=True, use_vocab=True, tokenizer_language='en')
-	# LABEL = data.Field(sequential=False, use_vocab=False)
-	# data_fields = [
-	# 	('NewsType', LABEL), ('NewsHeading', TEXT)  # , ('News', TEXT)
-	# ]
-	# trainset, testset = data.TabularDataset.splits(path=datadir, train='train.csv', test='test.csv', format='csv',
-	# 											   fields=data_fields)
-	# vec = vocab.Vectors('glove.6B.100d.txt', '../../data/glove_embedding/')
-	# TEXT.build_vocab(trainset, vectors=vec)
-	# LABEL.build_vocab(trainset)
-	modelfamily = datasets.dataset_to_modelfamily[dataset_name]
-	metadata = datasets.dataset_metadata[dataset_name]
-	# train_transform = datasets.modelfamily_to_transforms[modelfamily]['train'] #??? Is it right to use imagenet transformer for all other datasets in the family?
-	# test_transform = datasets.modelfamily_to_transforms[modelfamily]['test']
-	# trainset = dataset(train=True)
-	# testset = dataset(train=False)
-	# num_classes = metadata['num_classes']
-
-	# if params['train_subset'] is not None:
-	# 	idxs = np.arange(len(trainset))
-	# 	ntrainsubset = params['train_subset']
-	# 	idxs = np.random.choice(idxs, size=ntrainsubset, replace=False)
-	# 	trainset = Subset(trainset, idxs)
-
-	# ----------- Set up model
-	model_name = params['model_arch']
-	pretrained = params['pretrained']
 	ngrams = metadata['ngram']
 
-	#Use predownloaded dataset if available
+	# If dataset does not exist, download it and save it
 	datadir = datadir + '/' + dataset_name.lower() + '_csv'
 	train_data_path = os.path.join(datadir, dataset_name + "_ngrams_{}_train.data".format(ngrams))
 	test_data_path = os.path.join(datadir, dataset_name + "_ngrams_{}_test.data".format(ngrams))
@@ -132,25 +107,23 @@ def main():
 		print("Saving test data to {}".format(test_data_path))
 		torch.save(testset, test_data_path)
 	else:
-		# datadir = datadir + '/' + dataset_name.lower() + '_csv'  # path to already present dataset
 		print("Loading train data from {}".format(train_data_path))
 		trainset = torch.load(train_data_path)
 		print("Loading test data from {}".format(test_data_path))
 		testset = torch.load(test_data_path)
 
+	# Extract variables for model from the dataset
 	vocab_size = len(trainset.get_vocab())
 	params['num_classes'] = len(trainset.get_labels())
 	num_classes = params['num_classes']
 
-	# model = model_utils.get_net(model_name, n_output_classes=num_classes, pretrained=pretrained)
 	model = zoo.get_net(model_name, modelfamily, pretrained, vocab_size=vocab_size, embed_dim=embed_dim,
 						num_class=num_classes)
 	model = model.to(device)
-
-	# ----------- Train
 	model_utils.train_and_valid(trainset, testset, model, model_name, modelfamily, out_path, batch_size, lr, lr_gamma,
 								num_workers, device=device, num_epochs=num_epochs)
-	# Store arguments in json file
+
+	# Store arguments in json file. Maybe for the transfer set step?
 	params['created_on'] = str(datetime.now())
 	params_out_path = osp.join(out_path, 'params.json')
 	with open(params_out_path, 'w') as jf:
