bf2eac4d2be19f230fff1eeb48adc8f9138a2364
Yu De Lin <c01yuli@gpu01.cispa.saarland>
2020-01-17 10:01:25 +0100

    Remove the batch size restriction

--- knockoff/models/classification/LSTM_Attn.py
+++ knockoff/models/classification/LSTM_Attn.py
@@ -85,14 +85,15 @@ class AttentionModel(torch.nn.Module):
 		
 		input = self.word_embeddings(input_sentences)
 		input = input.permute(1, 0, 2)
-		if batch_size is None:
-			h_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size).cuda())
-			c_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size).cuda())
-		else:
-			h_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
-			c_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
-			
-		output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0)) # final_hidden_state.size() = (1, batch_size, hidden_size) 
+		# if batch_size is None:
+		# 	h_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size).cuda())
+		# 	c_0 = Variable(torch.zeros(1, self.batch_size, self.hidden_size).cuda())
+		# else:
+		# 	h_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
+		# 	c_0 = Variable(torch.zeros(1, batch_size, self.hidden_size).cuda())
+		
+		output, (final_hidden_state, final_cell_state) = self.lstm(input)
+		# output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0)) # final_hidden_state.size() = (1, batch_size, hidden_size) 
 		output = output.permute(1, 0, 2) # output.size() = (batch_size, num_seq, hidden_size)
 		
 		attn_output = self.attention_net(output, final_hidden_state)
