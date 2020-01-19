68427c6618227d375564d1ce25b611ab139428ca
Nikhil Chilwant <nikhilbchilwant@gmail.com>
2020-01-19 23:37:47 +0100

    Removed redundant code

--- knockoff/adversary/train.py
+++ knockoff/adversary/train.py
@@ -28,83 +28,13 @@ __maintainer__ = "Tribhuvanesh Orekondy"
 __email__ = "orekondy@mpi-inf.mpg.de"
 __status__ = "Development"
 
-
-class TransferSetImagePaths(ImageFolder):
-    """TransferSet Dataset, for when images are stored as *paths*"""
-
-    def __init__(self, samples, transform=None, target_transform=None):
-        self.loader = default_loader
-        self.extensions = IMG_EXTENSIONS
-        self.samples = samples
-        self.targets = [s[1] for s in samples]
-        self.transform = transform
-        self.target_transform = target_transform
-
-
-class TransferSetImages(Dataset):
-    def __init__(self, samples, transform=None, target_transform=None):
-        self.samples = samples
-        self.transform = transform
-        self.target_transform = target_transform
-
-        self.data = [self.samples[i][0] for i in range(len(self.samples))]
-        self.targets = [self.samples[i][1] for i in range(len(self.samples))]
-
-    def __getitem__(self, index):
-        img, target = self.data[index], self.targets[index]
-
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
-        img = Image.fromarray(img)
-
-        if self.transform is not None:
-            img = self.transform(img)
-
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-
-        return img, target
-
-    def __len__(self):
-        return len(self.data)
-
-
-def samples_to_transferset(samples, budget=None, transform=None, target_transform=None):
-    # Images are either stored as paths, or numpy arrays
-    sample_x = samples[0][0]
-    assert budget <= len(samples), 'Required {} samples > Found {} samples'.format(budget, len(samples))
-
-    if isinstance(sample_x, str):
-        return TransferSetImagePaths(samples[:budget], transform=transform, target_transform=target_transform)
-    elif isinstance(sample_x, np.ndarray):
-        return TransferSetImages(samples[:budget], transform=transform, target_transform=target_transform)
-    else:
-        raise ValueError('type(x_i) ({}) not recognized. Supported types = (str, np.ndarray)'.format(type(sample_x)))
-
-
-def get_optimizer(parameters, optimizer_type, lr=0.01, momentum=0.5, **kwargs):
-    assert optimizer_type in ['sgd', 'sgdm', 'adam', 'adagrad']
-    if optimizer_type == 'sgd':
-        optimizer = optim.SGD(parameters, lr)
-    elif optimizer_type == 'sgdm':
-        optimizer = optim.SGD(parameters, lr, momentum=momentum)
-    elif optimizer_type == 'adagrad':
-        optimizer = optim.Adagrad(parameters)
-    elif optimizer_type == 'adam':
-        optimizer = optim.Adam(parameters)
-    else:
-        raise ValueError('Unrecognized optimizer type')
-    return optimizer
-
-
 def main():
     parser = argparse.ArgumentParser(description='Train a model')
     # Required arguments
     parser.add_argument('model_dir', metavar='DIR', type=str, help='Directory containing transferset.pickle')
     parser.add_argument('model_arch', metavar='MODEL_ARCH', type=str, help='Model name')
     parser.add_argument('testdataset', metavar='DS_NAME', type=str, help='Name of test')
-    parser.add_argument('--budgets', metavar='B', type=str,
-                        help='Comma separated values of budgets. Knockoffs will be trained for each budget.')
+    parser.add_argument('--budgets', metavar='B', type=int)
     # Optional arguments
     parser.add_argument('-d', '--device_id', metavar='D', type=int, help='Device id. -1 for CPU.', default=0)
     parser.add_argument('-b', '--batch-size', type=int, default=64, metavar='N',
@@ -153,24 +83,10 @@ def main():
     num_classes = transferset_samples.num_classes
     print('=> found transfer set with {} samples, {} classes'.format(len(transferset_samples.data), num_classes))
 
-
-    # ----------- Clean up transfer (if necessary)
-    # if params['argmaxed']:
-    #     new_transferset_samples = []
-    #     print('=> Using argmax labels (instead of posterior probabilities)')
-    #     for i in range(len(transferset_samples)):
-    #         x_i, y_i = transferset_samples[i]
-    #         argmax_k = y_i.argmax()
-    #         # y_i_1hot = torch.zeros_like(y_i)
-    #         # y_i_1hot[argmax_k] = 1.
-    #         new_transferset_samples.append((x_i, argmax_k))
-    #     transferset_samples = new_transferset_samples
     # ----------- Set up testset
     dataset_name = params['testdataset']
     valid_datasets = datasets.__dict__.keys()
-    # modelfamily = datasets.dataset_to_modelfamily[dataset_name]
-    # transform = datasets.modelfamily_to_transforms[modelfamily]['test']
-    # See /knockoff/datasets/__init__.py for mapping
+
     modelfamily = datasets.dataset_to_modelfamily[dataset_name]  # e.g. 'classification'
     metadata = datasets.dataset_metadata[dataset_name]  # Relevant parameters for the task. e.g. 'ngram'
 
@@ -207,26 +123,15 @@ def main():
     # ----------- Set up model
     model_name = params['model_arch']
     pretrained = params['pretrained']
-    # model = model_utils.get_net(model_name, n_output_classes=num_classes, pretrained=pretrained)
 
-    # model = zoo.get_net(model_name, modelfamily, pretrained, num_classes=num_classes)
     model = zoo.get_net(model_name, modelfamily, pretrained, vocab_size=len(trainset.get_vocab()), embed_dim=params['embed_dim'],
                         num_class=num_classes)
 
     # ----------- Train
-    budgets = [int(b) for b in params['budgets'].split(',')]
+    # budget = params['budgets']
+    # assert budget <= len(transferset_samples.data * 8), 'Required {} samples > Found {} samples'.format(budget, len(transferset_samples.data))
 
-    for b in budgets:
-        np.random.seed(cfg.DEFAULT_SEED)
-        torch.manual_seed(cfg.DEFAULT_SEED)
-        torch.cuda.manual_seed(cfg.DEFAULT_SEED)
-
-        print('=> Training at budget = {}'.format(len(transferset_samples.data)))
-
-        optimizer = get_optimizer(model.parameters(), params['optimizer_choice'], **params)
-        print(params)
-
-        model_utils.train_and_valid_knockoff(transferset_samples.data, testset, model, model_name, modelfamily, device=device, **params)
+    model_utils.train_and_valid_knockoff(transferset_samples.data, testset, model, model_name, modelfamily, device=device, **params)
 
     # Store arguments
     params['created_on'] = str(datetime.now())
