662033c54b117826b7d32ad2401252217ee9b7f2
Yu De Lin <c01yuli@gpu01.cispa.saarland>
2020-01-21 17:26:13 +0100

    1. Add pad and pack sequence features. 2. Provide api for knockoff to import

--- knockoff/models/classification/RCNN.py
+++ knockoff/models/classification/RCNN.py
@@ -4,46 +4,50 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from torch.nn import functional as F
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+
+__all__ = ['rcnn']
 
 class RCNN(nn.Module):
-	def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_length, weights=None):
+	def __init__(self, output_size, hidden_size, 
+				 vocab_size, seq_len, embed_dim, weights=None):
 		super(RCNN, self).__init__()
 		
 		"""
 		Arguments
 		---------
-		batch_size : Size of the batch which is same as the batch_size of the data returned by the TorchText BucketIterator
 		output_size : 2 = (pos, neg)
 		hidden_sie : Size of the hidden_state of the LSTM
 		vocab_size : Size of the vocabulary containing unique words
-		embedding_length : Embedding dimension of GloVe word embeddings
+		embed_dim : Embedding dimension of GloVe word embeddings
 		weights : Pre-trained GloVe word_embeddings which we will use to create our word_embedding look-up table 
 		
 		"""
 		
-		self.batch_size = batch_size
 		self.output_size = output_size
 		self.hidden_size = hidden_size
 		self.vocab_size = vocab_size
-		self.embedding_length = embedding_length
+		self.embed_dim = embed_dim
+		self.seq_len = seq_len
 		
-		self.word_embeddings = nn.Embedding(vocab_size, embedding_length)# Initializing the look-up table.
+		self.word_embeddings = nn.Embedding(vocab_size, embed_dim)# Initializing the look-up table.
 		if weights:
 			self.word_embeddings.weight = \
 				nn.Parameter(weights, requires_grad=False) # Assigning the look-up table to the pre-trained GloVe word embedding.
 		self.dropout = 0.8
-		self.lstm = nn.LSTM(embedding_length, hidden_size, dropout=self.dropout, bidirectional=True)
-		self.W2 = nn.Linear(2*hidden_size+embedding_length, hidden_size)
+		self.lstm = nn.LSTM(embed_dim, hidden_size, batch_first=True,
+							dropout=self.dropout, bidirectional=True)
+		self.W2 = nn.Linear(2*hidden_size+embed_dim, hidden_size)
 		self.label = nn.Linear(hidden_size, output_size)
 		
-	def forward(self, input_sentence, batch_size=None):
+	def forward(self, input_sentence, input_lengths):
 	
 		""" 
 		Parameters
 		----------
 		input_sentence: input_sentence of shape = (batch_size, num_sequences)
-		batch_size : default = None. Used only for prediction on a single sentence after training (batch_size = 1)
-		
+		input_length: The original length of each sequence
+
 		Returns
 		-------
 		Output of the linear layer containing logits for positive & negative class which receives its input as the final_hidden_state of the LSTM
@@ -61,16 +65,14 @@ class RCNN(nn.Module):
 		dimension tensor of size (batch_size, hidden_size) and finally we map this to the output layer.
 
 		"""
-		input = self.word_embeddings(input_sentence) # embedded input of shape = (batch_size, num_sequences, embedding_length)
-		input = input.permute(1, 0, 2) # input.size() = (num_sequences, batch_size, embedding_length)
-		# if batch_size is None:
-		# 	h_0 = Variable(torch.zeros(2, self.batch_size, self.hidden_size).cuda()) # Initial hidden state of the LSTM
-		# 	c_0 = Variable(torch.zeros(2, self.batch_size, self.hidden_size).cuda()) # Initial cell state of the LSTM
-		# else:
-		# 	h_0 = Variable(torch.zeros(2, batch_size, self.hidden_size).cuda())
-		# 	c_0 = Variable(torch.zeros(2, batch_size, self.hidden_size).cuda())
-		output, (final_hidden_state, final_cell_state) = self.lstm(input)
-		# output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0))
+		pad = torch.empty(input_sentence.size(1), self.seq_len-input_sentence.size(0), dtype=torch.int64).fill_(1).cuda()
+		input_sentence = torch.cat((input_sentence.permute(1, 0), pad), 1)
+		input = self.word_embeddings(input_sentence) # embedded input of shape = (batch_size, num_sequences, embed_dim)
+		input = input.permute(1, 0, 2) 
+		packed_input = pack_padded_sequence(input, input_lengths)
+
+		output, (final_hidden_state, final_cell_state) = self.lstm(packed_input)
+		output, _ = pad_packed_sequence(output, total_length=self.seq_len)
 		
 		final_encoding = torch.cat((output, input), 2).permute(1, 0, 2)
 		y = self.W2(final_encoding) # y.size() = (batch_size, num_sequences, hidden_size)
@@ -80,3 +82,7 @@ class RCNN(nn.Module):
 		logits = self.label(y)
 		
 		return logits
+
+def rcnn(**kwargs):
+
+	return RCNN(**kwargs)
